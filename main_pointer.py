4248dd2219158117a50eec209463861d056d2ebf
dinamitrii <k_karaivanov@abv.bg>
2023-12-19 21:11:19 +0200

    commit

--- main_pointer.py
+++ main_pointer.py
@@ -1,5 +1,10 @@
+import copy
 import time
 import datetime
+from datetime import date
+from typing import List, Set
+
+import template_dict
 
 
 def age_calculator(date):
@@ -26,23 +31,32 @@ pointer_list_with_dictionaries_for_friends = [{
 
 current_time = time.time()
 time_string = time.ctime(current_time)
-print("Current time: ", time_string)
+print("Current time is: ", time_string)
 
 
-def full_info(na, ln, ni, bi, yo, ci, di, pc, add, ph) -> list:
+def full_info(na, ln, ni, yo, ci, di, pc, add, ph) -> list[date | str | int | None]:
     name = pointer_list_with_dictionaries_for_friends[0].get("name")
     lastname = pointer_list_with_dictionaries_for_friends[0].get("last_name")
     city = pointer_list_with_dictionaries_for_friends[0].get("city")
     district = pointer_list_with_dictionaries_for_friends[0].get("district")
     postal_code = pointer_list_with_dictionaries_for_friends[0].get("postal_code")
     address = pointer_list_with_dictionaries_for_friends[0].get("address")
-    birthday = pointer_list_with_dictionaries_for_friends[0].get("birthday")
+    # birthday = pointer_list_with_dictionaries_for_friends[0].get("birthday")
     phone = pointer_list_with_dictionaries_for_friends[0].get("phone")
     years_old = pointer_list_with_dictionaries_for_friends[0].get("years_old")
     nickname = pointer_list_with_dictionaries_for_friends[0].get("nickname")
 
-    result = [name, lastname, nickname, birthday, years_old, "Годишен", city, district, postal_code, address, phone]
+    result = [name, lastname, nickname, years_old, city, district, postal_code, address, phone]
     return result
 
 
-print(full_info(na=1, ln=2, ni=3, bi=4, yo=5, ci=6, di=7, pc=8, add=9, ph=10))
+print(full_info(na=1, ln=2, ni=3, yo=5, ci=6, di=7, pc=8, add=9, ph=10))
+
+
+def new_dict_add():
+    pointer_list_with_dictionaries_for_friends.append(dict(template_dict.new_dict))
+    print(pointer_list_with_dictionaries_for_friends)
+    return True
+
+
+new_dict_add()
---
+++ template_dict.py
@@ -0,0 +1,12 @@
+new_dict = {
+    "name": None,
+    "last_name": None,
+    "nickname": None,
+    "birthday": None,
+    "years_old": None,
+    "phone": None,
+    "city": None,
+    "district": None,
+    "postal_code": None,
+    "address": None
+}
